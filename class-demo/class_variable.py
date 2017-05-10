593ecc692ea89a9c01597940f87fd0220119d84c
hugoxia <hugoxia88@163.com>
2017-05-10 23:56:04 +0800

    class variable and instance variable

---
+++ class-demo/class_variable.py
@@ -0,0 +1,25 @@
+# coding: utf-8
+
+class A():
+    params = 0 # 类变量，也称为“静态变量”
+
+    def add(self):
+        A.params += 1
+
+
+if __name__ == "__main__":
+    a = A()  # 实例化对象1
+    b = A()  # 实例化对象2
+    print a.params  # 执行结果为: 0
+    print a.params  # 执行结果为: 0
+    print A.params # 执行结果为: 0
+
+    a.add()
+    print a.params  # 执行结果为: 1
+    print b.params  # 执行结果为: 1
+    print A.params # 执行结果为: 1
+
+    b.add()
+    print a.params # 执行结果为: 2
+    print b.params # 执行结果为: 2
+    print A.params # 执行结果为: 2
