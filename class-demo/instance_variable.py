532983c6aaf7d51aa1506b9d09b234658d99241f
hugoxia <hugoxia88@163.com>
2017-05-11 09:25:34 +0800

    add __init__

--- class-demo/instance_variable.py
+++ class-demo/instance_variable.py
@@ -1,7 +1,8 @@
 # coding: utf-8
 
 class A():
-    params = 0 # 类变量，也称为“静态变量”
+    def __init__(self):
+        self.params = 0
 
     def add(self):
         self.params += 1
@@ -12,14 +13,11 @@ if __name__ == "__main__":
     b = A()  # 实例化对象2
     print a.params  # 执行结果为: 0
     print b.params  # 执行结果为: 0
-    print A.params # 执行结果为: 0
 
     a.add()
     print a.params  # 执行结果为: 1
     print b.params  # 执行结果为: 0
-    print A.params # 执行结果为: 0
 
     b.add()
     print a.params # 执行结果为: 1
     print b.params # 执行结果为: 1
-    print A.params # 执行结果为: 0
